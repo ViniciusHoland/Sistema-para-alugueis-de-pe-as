00c4995337bc351d0a291c8d0454c6bf8556ff34
ViniciusDev <viniciusdev10@gmail.com>
2025-02-28 18:46:25 -0300

    creted schema User in prisma and connection with database mysql

--- routes/public/register.js
+++ routes/public/register.js
@@ -1,29 +1,55 @@
 import express from 'express';
 import bycrypt from 'bcrypt'
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const router = express.Router()
 
-router.post('/register', async (req,res) => {
+router.post('/register', async (req, res) => {
 
-    try{ 
+    try {
 
-        const { name , email ,password} = req.body
+        const { name, email, password } = req.body
 
-        const salts = 10 
+        const salts = 10
 
-        const hashPassword = await bycrypt.hash(password,salts)
+        const hashPassword = await bycrypt.hash(password, salts)
 
         console.log(name, email, hashPassword)
 
-        res.status(200).json({message: "Funcionando"})
+        res.status(200).json({ message: "Funcionando" })
 
-    } catch(err){
+    } catch (err) {
         console.error(err)
-        return res.status(500).json({error: 'error ao cadastrar usuario'})
+        return res.status(500).json({ error: 'error ao cadastrar usuario' })
     }
-  
+
 
 
 })
+
+
+/*router.get('/', () => {
+
+
+    async function testConnection() {
+        try {
+            await prisma.$connect();
+            console.log("✅ Conexão com o banco de dados estabelecida com sucesso!");
+        } catch (error) {
+            console.error("❌ Erro ao conectar com o banco de dados:", error);
+        } finally {
+            await prisma.$disconnect();
+        }
+    }
+
+    testConnection();
+
+
+})*/
+
+
 
 export default router 
