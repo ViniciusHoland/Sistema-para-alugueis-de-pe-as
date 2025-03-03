99d887902839dd05269c9bb7fe05f2a4da91466f
ViniciusDev <viniciusdev10@gmail.com>
2025-03-03 10:32:47 -0300

    created end point POST of Clientes

---
+++ routes/private/cliente.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+
+const router = express.Router()
+const prisma= new PrismaClient();
+
+router.post('/clientes/cadastro', async (req, res ) => {
+
+    try{
+
+        const { nome , email, telefone , endereco} = req.body
+
+        if(!nome || !email || !telefone || !endereco){
+            return res.status(400).json({ error: 'Por favor, preencha todos os campos' })  // Campos vazios
+        }
+
+        const user = await prisma.cliente.findUnique({where:{email: email}})
+    
+        if(user){
+            return res.status(400).json({ error: 'Email já cadastrado' })  // Email já cadastrado
+        }
+
+        const newCliente = await prisma.cliente.create({
+            data: {
+                nome,
+                email,
+                telefone,
+                endereco : {
+                    rua: req.body.endereco.rua,
+                    numero: req.body.endereco.numero,
+                    bairro: req.body.endereco.bairro
+                }
+            }
+        })
+
+        res.status(201).json({message: `Cliente ${newCliente.nome} cadastrado com sucesso`})
+
+    }catch (err){ 
+        console.error(err)
+        return res.status(500).json({ message: 'Erro ao cadastrar cliente' })
+    }
+
+})
+
+export default router
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import register from './routes/public/register.js'
 import login from './routes/public/login.js'
 import pecas from './routes/private/pecas.js'
 import auth  from './midllewares/auth.js'
+import cliente from './routes/private/cliente.js'
 
 const app = express()
 app.use(express.json())
@@ -12,6 +13,7 @@ dotenv.config()
 app.use(register)
 app.use(login)
 app.use(auth,pecas)
+app.use(auth,cliente)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
