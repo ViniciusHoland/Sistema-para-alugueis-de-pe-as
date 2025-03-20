9dd306a6131f0dca37c00133af3fbaadbe20bf9f
ViniciusDev <viniciusdev10@gmail.com>
2025-03-20 20:59:41 -0300

    update functions e return for frontend

--- routes/private/registro.js
+++ routes/private/registro.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import { number } from 'zod';
 
 const prisma = new PrismaClient()
 const router = express.Router()
@@ -8,28 +9,31 @@ router.post('/alugueis/registro', async (req , res ) => {
 
     try{
 
-        const {clienteId , idPeca , quantidade} = req.body
+        const {idCliente, idPeca , quantidade} = req.body
         let { dataInicio , dataFim } = req.body
 
-        if(!clienteId || !idPeca || !quantidade || !dataInicio  || !dataFim){
+
+        if(!idCliente || !idPeca || !quantidade || !dataInicio  || !dataFim){
             return res.status(400).json({ error: 'Por favor, preencha todos os campos obrigatórios' })
         }
+
+        const clienteId = parseInt(idCliente)
 
         if(!await prisma.cliente.findUnique({where: {id: clienteId}})){
             return res.status(404).json({ error: 'Cliente não encontrado' })
         }
 
-        const [dia, mes , ano] = dataInicio.split('/')
+        const [ano, mes,dia ] = dataInicio.split('-')
         const dataFormatInicio = `${ano}/${mes}/${dia}`
         dataInicio = new Date(dataFormatInicio)
 
-        const [diaF, mesF , anoF] = dataFim.split('/')
+        const [anoF, mesF, diaF] = dataFim.split('-')
         const dateFormatFim = `${anoF}/${mesF}/${diaF}`
         dataFim = new Date(dateFormatFim)
 
         const userId = req.userId
 
-        const peca = await prisma.peca.findUnique({where: { id : idPeca} })
+        const peca = await prisma.peca.findUnique({where: { id : parseInt(idPeca)} })
 
         if(!peca){
             return res.status(404).json({ error: 'Peça não encontrada' })
@@ -41,11 +45,12 @@ router.post('/alugueis/registro', async (req , res ) => {
         const nomedaPeca = peca.name.toString()
         const precoUnitarioPeca = peca.valorDiario.toFixed(2)
 
+        const quantidadePecas = parseInt(quantidade)
 
         const itensAluguel = {
             pecaId: peca.id,
             nomePeca : nomedaPeca,
-            quantidade,
+            quantidade: quantidadePecas,
             precoUnitario: precoUnitarioPeca
         }
 
@@ -67,7 +72,6 @@ router.post('/alugueis/registro', async (req , res ) => {
             }
         })
        
-
     
         res.status(201).json(newAluguel)
 
@@ -80,7 +84,7 @@ router.post('/alugueis/registro', async (req , res ) => {
 
 })
 
-router.get('/alugueis/:idCliente', async (req, res) => {
+router.get('/alugueis/cliente/:idCliente', async (req, res) => {
 
     try{
 
@@ -115,6 +119,10 @@ router.get('/alugueis/:idCliente', async (req, res) => {
 
         const dadosAlugueis = alugueis.map(aluguel => ({
 
+            idAluguel: aluguel.id,
+            nomeCliente: aluguel.cliente.nome,
+            endereco: aluguel.cliente.endereco,
+            telefone: aluguel.cliente.telefone,
             dataInicio: aluguel.dataInicio,
             dataFim: aluguel.dataFim,
             quantidadeDias: aluguel.quantidadeDias,
@@ -164,6 +172,7 @@ router.get('/alugueis', async (req, res) => {
 
             status: aluguel.status,
             idCliente: aluguel.cliente.id,
+            idAluguel: aluguel.id,
             nomeCliente: aluguel.cliente.nome,
             dataInicio: aluguel.dataInicio,
             dataFim: aluguel.dataFim,
@@ -210,60 +219,71 @@ router.put('/alugueis/:aluguelId', async (req, res) => {
             return res.status(404).json({ error: 'Aluguel não encontrado' })
         }
 
-
-        const {clienteId , idPeca , quantidade} = req.body
-        let { dataInicio , dataFim } = req.body
-
-        if(!clienteId || !idPeca || !quantidade || !dataInicio  || !dataFim){
-            return res.status(400).json({ error: 'Por favor, preencha todos os campos obrigatórios' })
+        if(!req.body.status === "fechado") {
+            const {clienteId , idPeca , quantidade} = req.body
+            let { dataInicio , dataFim } = req.body
+    
+            if(!clienteId || !idPeca || !quantidade || !dataInicio  || !dataFim){
+                return res.status(400).json({ error: 'Por favor, preencha todos os campos obrigatórios' })
+            }
+    
+    
+            if(!await prisma.cliente.findUnique({where: {id: clienteId}})){
+                return res.status(404).json({ error: 'Cliente não encontrado' })
+            }
+    
+            const peca= await prisma.peca.findUnique({where: {id: idPeca}})
+    
+            const userId = req.userId
+    
+            const [dia, mes , ano] = dataInicio.split('/')
+            const dataFormatInicio = `${ano}/${mes}/${dia}`
+            dataInicio = new Date(dataFormatInicio)
+    
+            const [diaF, mesF , anoF] = dataFim.split('/')
+            const dateFormatFim = `${anoF}/${mesF}/${diaF}`
+            dataFim = new Date(dateFormatFim)
+    
+            const totalDias= Math.ceil((dataFim - dataInicio) / (1000 * 60 * 60 * 24))
+    
+            const itensAluguel = {
+                pecaId: peca.id,
+                nomePeca : peca.name,
+                quantidade,
+                precoUnitario: peca.valorDiario
+            }
+    
+    
+            console.log(aluguel)
+    
+            const updateAluguel = await prisma.aluguel.update({
+                where: {id : idAluguel},
+                data: {
+                    clienteId,
+                    userId: userId,
+                    dataInicio,
+                    dataFim,
+                    quantidadeDias: totalDias,
+                    status : "aberto",
+                    aluguelItens: {
+                        create: itensAluguel
+                    }
+                }
+            })
+    
+            res.status(200).json(updateAluguel)
         }
-
-
-        if(!await prisma.cliente.findUnique({where: {id: clienteId}})){
-            return res.status(404).json({ error: 'Cliente não encontrado' })
-        }
-
-        const peca= await prisma.peca.findUnique({where: {id: idPeca}})
-
-        const userId = req.userId
-
-        const [dia, mes , ano] = dataInicio.split('/')
-        const dataFormatInicio = `${ano}/${mes}/${dia}`
-        dataInicio = new Date(dataFormatInicio)
-
-        const [diaF, mesF , anoF] = dataFim.split('/')
-        const dateFormatFim = `${anoF}/${mesF}/${diaF}`
-        dataFim = new Date(dateFormatFim)
-
-        const totalDias= Math.ceil((dataFim - dataInicio) / (1000 * 60 * 60 * 24))
-
-        const itensAluguel = {
-            pecaId: peca.id,
-            nomePeca : peca.name,
-            quantidade,
-            precoUnitario: peca.valorDiario
-        }
-
-
-        console.log(aluguel)
 
         const updateAluguel = await prisma.aluguel.update({
             where: {id : idAluguel},
             data: {
-                clienteId,
-                userId: userId,
-                dataInicio,
-                dataFim,
-                quantidadeDias: totalDias,
-                status : "aberto",
-                aluguelItens: {
-                    create: itensAluguel
+                    status: "fechado"
                 }
-            }
+            
         })
 
+       
         res.status(200).json(updateAluguel)
-
 
     } catch (err) {
         console.error(err)
